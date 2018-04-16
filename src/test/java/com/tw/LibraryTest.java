d92fc2b1fc23a3df0180adc546cae049a5bd6b53
HwyUee <13164619630@163.com>
2018-04-16 18:42:10 +0800

    build successful

--- src/test/java/com/tw/LibraryTest.java
+++ src/test/java/com/tw/LibraryTest.java
@@ -28,4 +28,22 @@ public class LibraryTest {
 
     }
 
+    @Test
+    public void testCheck_StuInfo_Format() throws Exception {
+        Library library = new Library();
+        String input1 = "张三,123,数学:76,语文:87,英语:65,编程:98";
+        assertTrue(library.check_StuInfo_Format(input1));
+        String input2 = "张三,123,数学:,语文:87,英语:65,编程:98";
+        assertFalse(library.check_StuInfo_Format(input2));
+    }
+
+    @Test
+    public void testCheck_StuID_Format() throws Exception {
+        Library library = new Library();
+        String input1 = "123,234,456";
+        assertTrue(library.check_StuID_Format(input1));
+        String input2 = "1.23,345";
+
+
+    }
 }
